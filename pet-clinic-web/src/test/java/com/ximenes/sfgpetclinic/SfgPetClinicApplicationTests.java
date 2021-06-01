4d9a6852dc4b8e9a72d5c583acbb386c1d636226
berkson <berksonx@yahoo.com.br>
2021-05-31 22:36:06 -0300

    funcionando Beleza.

--- pet-clinic-web/src/test/java/com/ximenes/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/ximenes/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,20 +1,14 @@
 package com.ximenes.sfgpetclinic;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class SfgPetClinicApplicationTests {
 
-    @Autowired
-    private ApplicationContext applicationContext;
-
     @Test
     void contextLoads() {
-        Assertions.assertNotNull(applicationContext);
+
     }
 
 }
