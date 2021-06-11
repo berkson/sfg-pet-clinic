553474e1edc71490b62ddfff3b218bc41317ca18
berkson <berksonx@yahoo.com.br>
2021-06-11 10:25:08 -0300

    Informação de endereço acrescentada na entidade Owner. closes #22

--- pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/models/Owner.java
+++ pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/models/Owner.java
@@ -9,8 +9,26 @@ import java.util.Set;
  */
 public class Owner extends Person {
 
+    private String address;
+    private String city;
     private Set<Pet> pets;
+    private String telephone;
 
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 
     public Set<Pet> getPets() {
         return pets;
@@ -19,4 +37,13 @@ public class Owner extends Person {
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
     }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 }
+
