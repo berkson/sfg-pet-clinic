5229a1d454f3ce41c4aa8f56e784907a84b578a4
berkson <berksonx@yahoo.com.br>
2021-06-11 09:45:00 -0300

    Criada a entidade de especialidades de veterinários e adicionada a veterinário. closes #21

---
+++ pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/models/Specialty.java
@@ -0,0 +1,18 @@
+package com.ximenes.sfgpetclinic.models;
+
+/**
+ * Created by Berkson Ximenes
+ * Date: 11/06/2021
+ * Time: 09:40
+ */
+public class Specialty extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/models/Vet.java
+++ pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/models/Vet.java
@@ -1,10 +1,21 @@
 package com.ximenes.sfgpetclinic.models;
 
+import java.util.Set;
+
 /**
  * Created by berkson
  * Date: 27/05/2021
  * Time: 22:09
  */
-public class Vet extends Person{
+public class Vet extends Person {
 
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
