a5459c83eed038f77fd44888663a5887de38bf6f
berkson <berksonx@yahoo.com.br>
2021-06-11 10:44:59 -0300

    PetTypeMapService implementado. closes #23

---
+++ pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,12 @@
+package com.ximenes.sfgpetclinic.services;
+
+import com.ximenes.sfgpetclinic.models.PetType;
+
+/**
+ * Created by Berkson Ximenes
+ * Date: 11/06/2021
+ * Time: 10:33
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,38 @@
+package com.ximenes.sfgpetclinic.services.map;
+
+import com.ximenes.sfgpetclinic.models.PetType;
+import com.ximenes.sfgpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+/**
+ * Created by Berkson Ximenes
+ * Date: 11/06/2021
+ * Time: 10:31
+ */
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
