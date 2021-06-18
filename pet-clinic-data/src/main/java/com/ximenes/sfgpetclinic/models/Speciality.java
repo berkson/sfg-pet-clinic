b22ebc0c138b14d2bd7989232f11bb5b348145a6
Berkson <berksonx@yahoo.com.br>
2021-06-17 22:09:45 -0300

    Especialidade e Veterinários convertidos em entidades JPA . closes #30

--- pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/models/Speciality.java
+++ pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/models/Speciality.java
@@ -1,11 +1,19 @@
 package com.ximenes.sfgpetclinic.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by Berkson Ximenes
  * Date: 11/06/2021
  * Time: 09:40
  */
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/models/Vet.java
+++ pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/models/Vet.java
@@ -1,5 +1,6 @@
 package com.ximenes.sfgpetclinic.models;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,8 +9,13 @@ import java.util.Set;
  * Date: 27/05/2021
  * Time: 22:09
  */
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialties = new HashSet<>();
 
     public Set<Speciality> getSpecialties() {
