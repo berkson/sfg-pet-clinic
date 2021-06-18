223f896420991cc8a06eefc18366f8a359d3e2c0
Berkson <berksonx@yahoo.com.br>
2021-06-17 21:29:02 -0300

    Base entity implementado na JPA. closes #28

--- pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/models/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/models/BaseEntity.java
@@ -1,5 +1,9 @@
 package com.ximenes.sfgpetclinic.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
@@ -7,7 +11,16 @@ import java.io.Serializable;
  * Date: 31/05/2021
  * Time: 22:56
  */
+
+/*
+@MappedSuperclass - Serve para indicar à JPA que não deve ser criada uma tabela para essa entidade,
+pois ela será herdada por outras entidades
+ */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
