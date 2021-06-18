b7cd9bc234864c5dd26e0384e6fd76dbc803eed5
Berkson <berksonx@yahoo.com.br>
2021-06-17 22:55:33 -0300

    Pet e Visit convertidos em entidades JPA . closes #31

--- pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/models/Pet.java
+++ pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/models/Pet.java
@@ -1,7 +1,10 @@
 package com.ximenes.sfgpetclinic.models;
 
+import org.hibernate.annotations.Cascade;
+
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Set;
 
 /**
  * Created by berkson
@@ -25,6 +28,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits;
 
     public String getName() {
         return name;
@@ -56,5 +62,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
--- pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/models/Visit.java
+++ pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/models/Visit.java
@@ -1,5 +1,6 @@
 package com.ximenes.sfgpetclinic.models;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
@@ -7,10 +8,18 @@ import java.time.LocalDate;
  * Date: 11/06/2021
  * Time: 09:29
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
