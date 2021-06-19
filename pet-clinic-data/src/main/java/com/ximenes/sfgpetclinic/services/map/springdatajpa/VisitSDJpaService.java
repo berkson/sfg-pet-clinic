f9cda0d6925aeaddd9084501b5fe04428fc56d9e
berkson <berksonx@yahoo.com.br>
2021-06-18 22:24:03 -0300

    Create spring data jpa service for visit. closes #41

---
+++ pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/services/map/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,53 @@
+package com.ximenes.sfgpetclinic.services.map.springdatajpa;
+
+import com.ximenes.sfgpetclinic.models.Visit;
+import com.ximenes.sfgpetclinic.repositories.VisitRepository;
+import com.ximenes.sfgpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Berkson Ximenes
+ * Date: 18/06/2021
+ * Time: 22:20
+ */
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
