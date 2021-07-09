3f213723e0f45b11d4cd5e0bd76e571becb0fbd4
berkson <berksonx@yahoo.com.br>
2021-07-08 21:07:55 -0300

    Acrescentando builder patter ao PetType.java. #50

--- pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/models/PetType.java
+++ pet-clinic-data/src/main/java/com/ximenes/sfgpetclinic/models/PetType.java
@@ -15,10 +15,15 @@ import javax.persistence.Table;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
+
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
 
     @Column(name = "name")
     private String name;
