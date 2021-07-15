5a99279a966a8e7ae9a7d78188f1f62e4661009c
berkson <berksonx@yahoo.com.br>
2021-07-14 23:24:15 -0300

    criado o endpoint de veterinários. Closes #63

--- pet-clinic-web/src/main/java/com/ximenes/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/ximenes/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.ximenes.sfgpetclinic.controllers;
 
+import com.ximenes.sfgpetclinic.models.Vet;
 import com.ximenes.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 /**
  * Created by berkson
@@ -25,5 +30,11 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody
+    Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
